f00f787b691322768d8495acbba111befbefc577
Vishnu <vishnu@nuchange.com>
2022-06-22 18:55:24 +0530

    Update get_concept_name function

---
+++ liquibase/sql_files/updated_get_concept_name.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION IF EXISTS get_concept_name;
+GO
+CREATE FUNCTION get_concept_name(concept_id_str varchar(50))
+RETURNS varchar(255)
+DETERMINISTIC
+BEGIN
+    DECLARE con_name varchar(255);
+    SELECT coalesce((select name from concept_name where concept_id =  cast(concept_id_str as UNSIGNED) and concept_name_type = 'SHORT' and locale = 'en' and voided = 0),
+                    (select name from concept_name where concept_id =  cast(concept_id_str as UNSIGNED) and concept_name_type = 'FULLY_SPECIFIED' and locale = 'en' and voided = 0)) into con_name;
+    IF con_name = 'Yes' THEN
+        SET con_name = 'true';
+    ELSEIF con_name = 'No' THEN
+        SET con_name = 'false';
+    End IF;
+    RETURN con_name;
+END
+GO
